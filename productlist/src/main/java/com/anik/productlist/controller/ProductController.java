c4996bf1ace6fcbd7af86335139cbe73fa007cae
Md. Nazmul Hasan Anik <nazzmul.anik@gmail.com>
2026-02-16 09:31:14 +0600

    create get all, get by id, update by id, delete by id api for products

--- productlist/src/main/java/com/anik/productlist/controller/ProductController.java
+++ productlist/src/main/java/com/anik/productlist/controller/ProductController.java
@@ -5,10 +5,9 @@ import com.anik.productlist.service.ProductService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
@@ -20,5 +19,29 @@ public class ProductController {
     @PostMapping
     public ResponseEntity<ProductDTO> createProduct(@RequestBody ProductDTO productDTO){
         return new ResponseEntity<>(productService.createProduct(productDTO), HttpStatus.CREATED);
+    }
+
+    // Get All Product
+    @GetMapping
+    public List<ProductDTO> getAllProduct(){
+        return productService.getAllProduct();
+    }
+
+    // Get Product by Id
+    @GetMapping("/{id}")
+    public ProductDTO getProductById(@PathVariable Long id){
+        return productService.getProductById(id);
+    }
+
+    // Update product by Id
+    @PutMapping("/{id}")
+    public ProductDTO updateProductById(@PathVariable Long id, @RequestBody ProductDTO productDTO){
+        return productService.updateProductById(id, productDTO);
+    }
+
+    // Delete Product by Id
+    @DeleteMapping("/{id}")
+    public String deleteProductById(@PathVariable Long id){
+        return productService.deleteProductById(id);
     }
 }
--- productlist/src/main/java/com/anik/productlist/service/ProductService.java
+++ productlist/src/main/java/com/anik/productlist/service/ProductService.java
@@ -9,17 +9,54 @@ import com.anik.productlist.repository.ProductRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class ProductService {
     private ProductRepository productRepository;
     private CategoryRepository categoryRepository;
+
+    // Create Product
     public ProductDTO createProduct(ProductDTO productDTO){
 
         Category category = categoryRepository.findById(productDTO.getCategoryId())
-                .orElseThrow(()-> new RuntimeException("Category Not Found."));
+                .orElseThrow(()-> new RuntimeException("Category not found!"));
         Product product = ProductMapper.toProductEntity(productDTO, category);
         product = productRepository.save(product);
         return ProductMapper.toProductDTO(product);
+    }
+
+    // Get all product
+    public List<ProductDTO> getAllProduct(){
+        return productRepository.findAll()
+                .stream().map(ProductMapper::toProductDTO).toList();
+    }
+
+    // Get Product by Id
+    public ProductDTO getProductById(Long id){
+        Product product = productRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Product not found!"));
+        return ProductMapper.toProductDTO(product);
+    }
+
+    // Update product by Id
+    public ProductDTO updateProductById(Long id, ProductDTO productDTO){
+        Product product = productRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("Product not found!"));
+        Category category = categoryRepository.findById(productDTO.getCategoryId())
+                .orElseThrow(()-> new RuntimeException("Category not found!"));
+        product.setName(productDTO.getName());
+        product.setDescription(productDTO.getDescription());
+        product.setPrice(productDTO.getPrice());
+        product.setCategory(category);
+        productRepository.save(product);
+        return ProductMapper.toProductDTO(product);
+    }
+
+    // Delete Product by Id
+    public String deleteProductById(Long id){
+        productRepository.deleteById(id);
+        return "Product "+id+" has been deleted successfully!";
     }
 }
